7180de45abad3fedb9cfaee5f8387f7d4d884aad
YusaCan <ahmedyusacan@gmail.com>
2021-07-26 18:06:13 +0300

    .

---
+++ Catch it!/Assets/Scripts/BasketMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class BasketMovement : MonoBehaviour
+{
+
+    void Update()
+    {
+
+    }
+
+}
