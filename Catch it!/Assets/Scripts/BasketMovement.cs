d13a5d6b39adec372d9594e0d51b483d0a5474eb
YusaCan <ahmedyusacan@gmail.com>
2021-07-27 12:04:37 +0300

    BasketMovement Script
    Makes player swipe the basket right and left

--- Catch it!/Assets/Scripts/BasketMovement.cs
+++ Catch it!/Assets/Scripts/BasketMovement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class BasketMovement : MonoBehaviour
 {
-
-    void Update()
+    public float swipeSpeed;
+    Vector3 lastMousePosition;
+    private void OnMouseDown()
     {
-
+        lastMousePosition = Input.mousePosition;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 delta = Input.mousePosition - lastMousePosition;
+        Vector3 pos = transform.position;
+        pos.x += delta.x * swipeSpeed;
+        transform.position = pos;
+        lastMousePosition = Input.mousePosition;
     }
 
 }
