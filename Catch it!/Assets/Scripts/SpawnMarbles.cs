3d44efa6080fed07323712fee6dd0f7faf48edc4
YusaCan <ahmedyusacan@gmail.com>
2021-07-29 10:51:44 +0300

    Update SpanwMarble

--- Catch it!/Assets/Scripts/SpawnMarbles.cs
+++ Catch it!/Assets/Scripts/SpawnMarbles.cs
@@ -9,18 +9,18 @@ public class SpawnMarbles : MonoBehaviour
     private float spawnPosY = 6;
     private float spawnPosZ = -1.915f;
     private float waitingTime = 1f;
-    private float time;
+    private float localTime;
+    public float globalTime;
     private void Start()
     {
-        time = 0f;
+        localTime = 0f;
+        globalTime = 0f;
     }
     void FixedUpdate()
     {
-        time += Time.deltaTime;
-        if(time >= 0.5f)
-        {
-            StartCoroutine(Spawn());
-        }
+        globalTime += Time.deltaTime;
+        localTime += Time.deltaTime;
+        TimeToSpawn();
         
     }
 
@@ -35,7 +35,50 @@ public class SpawnMarbles : MonoBehaviour
             marble.transform.position = spawnPos;
             marble.transform.rotation = marble.transform.rotation;
             marble.SetActive(true);
+        }    
+    }
+
+    public void TimeToSpawn()
+    {
+        if (localTime >= 3f && globalTime < 15)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
         }
-        
+        else if(localTime >= 2f && globalTime > 15 && globalTime < 35)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 1.5f && globalTime > 35 && globalTime < 70)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 1f && globalTime > 70 && globalTime < 130)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 0.75f && globalTime > 130 && globalTime < 160)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 0.5f && globalTime > 160 && globalTime < 190)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 0.25f && globalTime > 190 && globalTime < 220)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
+        else if (localTime >= 0.1f && globalTime > 220)
+        {
+            StartCoroutine(Spawn());
+            localTime = 0f;
+        }
     }
 }
