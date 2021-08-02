58082cf8eac8c58b28e97ca6299e4949e4043b6e
YusaCan <ahmedyusacan@gmail.com>
2021-08-02 16:09:09 +0300

    Updated BasketDontGetAway Script
    Because of new responsive game objects

--- Catch it!/Assets/Scripts/BasketDontGetAway.cs
+++ Catch it!/Assets/Scripts/BasketDontGetAway.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BasketDontGetAway : MonoBehaviour
 {
-    private float xBound = 2.1f;
+    //private float xBound = 2.1f;
+
+    private RectOffset bdr;
+
+    private void OnGUI()
+    {
+        bdr = GUI.skin.button.border;
+    }
     void Update()
     {
-        if (transform.position.x < -xBound)
+        if (transform.position.x < -bdr.left / 2.4f)
         {
-            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-bdr.left / 2.4f, transform.position.y, transform.position.z);
         }
-        else if (transform.position.x > xBound)
+        else if (transform.position.x > bdr.right / 2.4f)
         {
-            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+            transform.position = new Vector3(bdr.right / 2.4f, transform.position.y, transform.position.z);
         }
     }
 }
