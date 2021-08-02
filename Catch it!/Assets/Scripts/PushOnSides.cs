9dcef82e995d2881ac66f3dcad33d0b55e8afedf
YusaCan <ahmedyusacan@gmail.com>
2021-08-02 16:08:39 +0300

    Updated PushOnSides
    Because of new responsive game objects

--- Catch it!/Assets/Scripts/PushOnSides.cs
+++ Catch it!/Assets/Scripts/PushOnSides.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class PushOnSides : MonoBehaviour
 {
     private Rigidbody marbleRigidbody;
-    private float xBound = 2.3f;
+    //private float xBound = 2.3f;
     [SerializeField] float forceSpeed;
+
     public void Start()
     {
+        
         marbleRigidbody = GetComponent<Rigidbody>();
     }
 
+    private void OnGUI()
+    {
+        RectOffset bdr;
+        bdr = GUI.skin.button.border;
+        Debug.Log("Left: " + bdr.left + " Right: " + bdr.right);
+
+        if (transform.position.x > bdr.right / 2.5f)
+        {
+            marbleRigidbody.AddForce(Vector3.left * forceSpeed, ForceMode.Impulse);
+        }
+        else if (transform.position.x < -bdr.left / 2.5f)
+        {
+            marbleRigidbody.AddForce(Vector3.right * forceSpeed, ForceMode.Impulse);
+        }
+    }
+
+/*
     public void FixedUpdate()
     {
         PushMarble();
     }
     public void PushMarble()
     {
-
+        
         if(transform.position.x > xBound)
         {
             marbleRigidbody.AddForce(Vector3.left * forceSpeed, ForceMode.Impulse);
@@ -28,4 +47,5 @@ public class PushOnSides : MonoBehaviour
             marbleRigidbody.AddForce(Vector3.right * forceSpeed, ForceMode.Impulse);
         }
     }
+*/
 }
