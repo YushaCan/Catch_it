4c6b5de0de952d203fa166ffeb75b377767dbc89
YusaCan <ahmedyusacan@gmail.com>
2021-08-04 12:27:28 +0300

    Updated GameManager Script
    New code blocks added for next level UI and function.

--- Catch it!/Assets/Scripts/GameManager.cs
+++ Catch it!/Assets/Scripts/GameManager.cs
@@ -8,10 +8,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public MarbleCollector score;
-    public HeartController heart;
+    //public HeartController heart;
     public SpawnMarbles SpawnTime;
 
-    public TextMeshProUGUI collectText; 
+    public TextMeshProUGUI nextLevelText;
+    public Button nextLevelButton;
+    public TextMeshProUGUI collectText;
+    public LevelGameModeDestroyer levelGameMode;
+    public SpawnMarbles spawnMarbles;
 
     public LevelDesign level;
     public TextMeshProUGUI gameOverText;
@@ -27,27 +31,41 @@ public class GameManager : MonoBehaviour
     {
         collectText.text = score.pickedMarbles + "/" + level.marbleAmountToCollect;
 
-        if (score.pickedMarbles == level.marbleAmountToCollect)
+        if (levelGameMode.losingMarble >= 1)
         {
             gameOver = true;
             Debug.Log("GAME OVER!");
             restartButton.gameObject.SetActive(true);
             gameOverText.gameObject.SetActive(true);
-            
+
+        }
+
+        if (score.pickedMarbles == level.marbleAmountToCollect)
+        {
+            gameOver = true;
+            Debug.Log("Next Level!");
+            nextLevelButton.gameObject.SetActive(true);
+            nextLevelText.gameObject.SetActive(true);          
         }
     }
     public void RestartGame()
     {
-        
+        spawnMarbles.marbleCounter = 1;
+        levelGameMode.losingMarble = 0;
         score.score = 0;
         score.pickedMarbles = 0;
-        heart.heart1.SetActive(true);
-        heart.heart2.SetActive(true);
-        heart.heart3.SetActive(true);
+        //heart.heart1.SetActive(true); This lines for the Infinity Game 
+        //heart.heart2.SetActive(true);
+        //heart.heart3.SetActive(true);
         SpawnTime.globalTime = 0;
         SpawnTime.localTime = 0;
         gameOver = false;
         restartButton.gameObject.SetActive(false);
         gameOverText.gameObject.SetActive(false);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
