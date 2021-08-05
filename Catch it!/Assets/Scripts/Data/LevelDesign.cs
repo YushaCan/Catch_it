92aa609a5efdd0c02158c2af3112c2ff7b4d20b5
YusaCan <ahmedyusacan@gmail.com>
2021-08-05 11:16:30 +0300

    Updated LevelDesign ScriptableObject

--- Catch it!/Assets/Scripts/Data/LevelDesign.cs
+++ Catch it!/Assets/Scripts/Data/LevelDesign.cs
@@ -7,6 +7,8 @@ public class LevelDesign : ScriptableObject
 {
     public GameObject obstacle;
     public GameObject marble;
+    public int level;
     public int marbleAmountToCollect;
+    public int marbleLooseCount;
     public Vector3[] spawnPoints;
 }
