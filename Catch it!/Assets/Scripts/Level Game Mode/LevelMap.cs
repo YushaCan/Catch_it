0e262cd929138580e04c245b7fed380f6a09fa71
YusaCan <ahmedyusacan@gmail.com>
2021-08-05 11:16:03 +0300

    Updated LevelMap Script

--- Catch it!/Assets/Scripts/Level Game Mode/LevelMap.cs
+++ Catch it!/Assets/Scripts/Level Game Mode/LevelMap.cs
@@ -9,19 +9,24 @@ public class LevelMap : MonoBehaviour
     
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
-    public Button pauseButton;
-    public Button resumeButton;
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+    public GameObject marbleCollectText;
 
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        pauseButton.SetActive(true);
+        marbleCollectText.SetActive(true);
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
     public void Pause() 
     {
         pauseMenuUI.SetActive(true);
+        pauseButton.SetActive(false);
+        marbleCollectText.SetActive(false);
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
