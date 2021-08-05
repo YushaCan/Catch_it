f99150105c14ada212cf10bb23d6d7c2f7a252c1
YusaCan <ahmedyusacan@gmail.com>
2021-08-05 18:44:25 +0300

    Create MarbleFeatures ScriptableObject

---
+++ Catch it!/Assets/Scripts/Data/MarbleFeatures.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Marble" , menuName = "Marble")]
+public class MarbleFeatures : ScriptableObject
+{
+    public GameObject Marble;
+    public string MarbleName;
+}
