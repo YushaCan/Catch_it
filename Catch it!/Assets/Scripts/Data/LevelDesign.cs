794ec74391acbcc04111edb99ebbfaa75a056152
YusaCan <ahmedyusacan@gmail.com>
2021-08-09 17:40:37 +0300

    Update LevelDesign Script

--- Catch it!/Assets/Scripts/Data/LevelDesign.cs
+++ Catch it!/Assets/Scripts/Data/LevelDesign.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Level ." , menuName = "Level")]
 public class LevelDesign : ScriptableObject
 {
     public GameObject obstacle;
     public GameObject marble;
+    
+    public GameObject image;
+    public bool isLevelCompleted = false;
+    
     public int level;
     public int marbleAmountToCollect;
     public int marbleLooseCount;
