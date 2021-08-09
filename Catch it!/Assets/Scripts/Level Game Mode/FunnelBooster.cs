797c69e5b310a88783866a2e307cb174f80530e1
YusaCan <ahmedyusacan@gmail.com>
2021-08-09 17:41:17 +0300

    Created FunnelBooster Script
    This script is for funnel booster to use

---
+++ Catch it!/Assets/Scripts/Level Game Mode/FunnelBooster.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunnelBooster : MonoBehaviour
+{
+    public LevelDesign level;
+    public int funnelBoosterAmount;
+    private float funnelBoosterTime = 10f;
+    private void Start()
+    {
+        funnelBoosterAmount = level.funnelBooster;
+        gameObject.SetActive(false);
+    }
+
+
+    public void UseFunnelBooster()
+    {
+        IEnumerator FunnelBooster()
+        {    
+            funnelBoosterAmount--;
+            yield return new WaitForSeconds(funnelBoosterTime);
+            gameObject.SetActive(false);
+        }
+
+        if (funnelBoosterAmount > 0)
+        {
+            gameObject.SetActive(true);
+            Debug.Log("FUNNEL APPEARED");
+            StartCoroutine(FunnelBooster());
+
+        }
+    }
+}
