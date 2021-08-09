d2d96814f33a1d1e88372a9c9324d152d015aeb6
YusaCan <ahmedyusacan@gmail.com>
2021-08-09 17:41:43 +0300

    Updated DynamicListView Script

--- Catch it!/Assets/Scripts/Level Game Mode/DynamicListView.cs
+++ Catch it!/Assets/Scripts/Level Game Mode/DynamicListView.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DynamicListView : MonoBehaviour
 {
-    public GameObject listItemPrefab;
     public Transform listItemHolder;
-    public int numOfListItems;
+    //public int numOfListItems;
     public List<GameObject> images;
-
+    public List<LevelDesign> levels;
+    public GameManager gameManager;
     private void Start()
     {
-        for(int i = 0; i < numOfListItems; i++)
+        for(int i = 0; i < levels.Count; i++)
         {
-            Instantiate(listItemPrefab, listItemHolder);
-            images.Add(listItemPrefab);
+            Instantiate(levels[i].image, listItemHolder);
+            images.Add(levels[i].image);
         }
-    }
-
-
-    public void ChangeColor()
-    {
-        //images[LevelChange.previousLevel].GetComponent<Image>().color = new Color32(0,255,19,255);
     }
 }
