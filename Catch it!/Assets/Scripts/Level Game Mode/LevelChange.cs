c32b48b5e9b03f2b6932d30661d6cd7da7a048f4
YusaCan <ahmedyusacan@gmail.com>
2021-08-09 17:42:18 +0300

    .

--- Catch it!/Assets/Scripts/Level Game Mode/LevelChange.cs
+++ Catch it!/Assets/Scripts/Level Game Mode/LevelChange.cs
@@ -6,16 +6,6 @@ using UnityEngine.UI;
 
 public class LevelChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
