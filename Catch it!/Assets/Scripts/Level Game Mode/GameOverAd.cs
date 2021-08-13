fd96806719d522e6a228e25a266a964bf0806431
YusaCan <ahmedyusacan@gmail.com>
2021-08-13 11:58:19 +0300

    Created GameOverAd Script
    This scripts allows us to put ads on the gameover screen (Rewarded)

---
+++ Catch it!/Assets/Scripts/Level Game Mode/GameOverAd.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameOverAd : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public static bool closeAd = false;
+    public static bool haveEarned = true;
+    private void OnEnable()
+    {
+        StartCoroutine(AdBegins());
+    }
+    public IEnumerator AdBegins()
+    {
+        gameObject.SetActive(true);
+        int time = 4;
+        while(time > 0)
+        {
+            time--;
+            timerText.text = "AD Begins In " + time;
+            yield return new WaitForSeconds(1);        
+        }
+        if(time == 0 && haveEarned)
+        {
+            closeAd = true;
+            Debug.Log("YOU EARN 10 GOLDS");
+        }
+    }
+
+    public void DecidedNotToWatch()
+    {
+        closeAd = true;
+        haveEarned = false;
+    }
+}
